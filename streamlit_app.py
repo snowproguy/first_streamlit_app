6b77ea16c2e8aab68e4881a28dc94113a148bad9
Guy Harris <111207078+snowproguy@users.noreply.github.com>
2022-08-14 16:36:29 -0400

    Update streamlit_app.py
    correct except URLError line indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ try:
       # output this to the screen as a table:
       streamlit.dataframe(fruityvice_normalized)
   
-  except URLError as e:
-      streamlit.error()
+except URLError as e:
+    streamlit.error()
   
 streamlit.write('The user entered ', fruit_choice)
 
