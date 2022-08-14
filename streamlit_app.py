4a874cbeac5f1b7a56d328f98935a7a1ae054d48
Guy Harris <111207078+snowproguy@users.noreply.github.com>
2022-08-14 16:05:03 -0400

    Update streamlit_app.py
    commit to test control of flow issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,11 +38,10 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+#streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
 #Query some data:
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
@@ -51,3 +50,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list value ('from streamlit')")
