37f14feb929dd2bebdacede9b99e02d64a38b6a0
Guy Harris <111207078+snowproguy@users.noreply.github.com>
2022-08-14 17:01:29 -0400

    Update streamlit_app.py
    corrected function in def of function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # Create the function code block:
-def get fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   # take the json version of the response and normalize it: 
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
