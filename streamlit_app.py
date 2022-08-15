825565b8269f33b8860e02927ede246ea5a1c437
Guy Harris <111207078+snowproguy@users.noreply.github.com>
2022-08-14 22:36:58 -0400

    Update streamlit_app.py
    add second cnx_close statement and comment out final stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,10 +68,11 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
  
-streamlit.write('Thanks for adding ', add_my_fruit)
-streamlit.stop()
+# streamlit.write('Thanks for adding ', add_my_fruit)
+# streamlit.stop()
 # my_cur = my_cnx.cursor()
 # my_cur.execute("select * from fruit_load_list")
 
